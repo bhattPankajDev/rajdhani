cbdad992a5b6445e692f93a0387cc6ec361db687
Pankaj Bhatt <pankajbhatt@LPT-PANKAJ-MAC.local>
2022-09-19 17:51:38 +0530

    config changes done

--- rajdhani/config.py
+++ rajdhani/config.py
@@ -2,8 +2,8 @@ import os
 
 secret_key = os.getenv("SECRET_KEY", "helloworld")
 
- # Feature Flags to enable features incrementally
- # Note: Please enable these flags only when the task asks you to do
+# Feature Flags to enable features incrementally
+# Note: Please enable these flags only when the task asks you to do
 
 flag_homepage = False
 flag_show_schedule_link = False
@@ -11,9 +11,9 @@ flag_ticketclass_in_search = False
 flag_search_filters = False
 flag_login = False
 flag_bookings = False
-
-## Database configurations
-## Note: Please do not modify these
+flag_homepage = True
+# Database configurations
+# Note: Please do not modify these
 
 db_path = "trains.db"
 db_uri = f"sqlite:///{db_path}"
