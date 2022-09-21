0856e598b9d85e1135a99df49141dd2204bb896d
Pankaj Bhatt <pankajbhatt@LPT-PANKAJ-MAC.local>
2022-09-21 16:13:01 +0530

    Ticket Booking  implementation Task 7

--- rajdhani/app.py
+++ rajdhani/app.py
@@ -18,6 +18,7 @@ app.secret_key = config.secret_key
 def add_context():
     return dict(config=config)
 
+
 @app.route("/")
 def index():
     from_station_code = request.args.get("from_station_code")
@@ -42,21 +43,25 @@ def index():
     today = datetime.today().strftime("%Y-%m-%d")
     return render_template("index.html", trains=trains, args=request.args, today=today)
 
+
 @app.route("/trains/<number>")
 def schedule(number):
     schedule = db.get_schedule(number)
     return render_template("schedule.html", schedule=schedule, train_number=number)
+
 
 @app.route("/api/flags")
 def api_flags():
     flags = {k: v for k, v in config.__dict__.items() if k.startswith("flag_")}
     return jsonify(flags)
 
+
 @app.route("/api/stations")
 def api_stations():
     q = request.args.get("q")
     stations = db.search_stations(q)
     return jsonify(stations)
+
 
 @app.route("/api/search")
 def api_search():
@@ -76,6 +81,7 @@ def api_search():
     trains = [dict(t) for t in trains]
     return jsonify(trains)
 
+
 @app.route("/search")
 def search():
     from_station = request.args.get("from")
@@ -85,16 +91,18 @@ def search():
 
     trains = db.search_trains(from_station, to_station, ticket_class, date)
     return render_template("search.html",
-        trains=trains,
-        from_station=from_station,
-        to_station=to_station,
-        ticket_class=ticket_class,
-        date=date)
+                           trains=trains,
+                           from_station=from_station,
+                           to_station=to_station,
+                           ticket_class=ticket_class,
+                           date=date)
+
 
 @app.route("/db/reset")
 def reset_db():
     db_ops.reset_db()
     return "ok"
+
 
 @app.route("/db/exec")
 def exec_db():
@@ -104,6 +112,7 @@ def exec_db():
         "columns": columns,
         "rows": rows
     }
+
 
 @app.route("/data-explorer")
 def explore_data():
@@ -121,6 +130,7 @@ def explore_data():
         error=error, columns=columns, rows=rows, query=q,
     )
 
+
 @app.route("/progress")
 def progress():
     url = urlparse(request.base_url)
@@ -129,15 +139,17 @@ def progress():
 
     return redirect(redirect_url, code=302)
 
+
 @app.route("/book-ticket", methods=["GET", "POST"])
 def book_ticket_page():
     if request.method == "POST":
-        db.book_ticket(train_number=request.form.get("train"),
-                       ticket_class=request.form.get("class"),
-                       departure_date=request.form.get("date"),
-                       passenger_name=request.form.get("passenger_name"),
-                       passenger_email=request.form.get("passenger_email"))
-
+        booking = db.book_ticket(train_number=request.form.get("train"),
+                                 ticket_class=request.form.get("class"),
+                                 date=request.form.get("date"),
+                                 passenger_name=request.form.get(
+                                     "passenger_name"),
+                                 passenger_email=request.form.get("passenger_email"))
+        notifications.send_booking_confirmation_email(booking)
         return redirect("/thank-you")
     else:
         email = auth.get_logged_in_user_email()
@@ -146,21 +158,16 @@ def book_ticket_page():
         ticket_class = request.args.get("class")
         date = request.args.get("date")
 
-    booking = db.book_ticket(
-                train_number=request.args.get("train"),
-                ticket_class=request.args.get("class"),
-                date=request.args.get("date"),
-                passenger_name=request.args.get("passenger_name"),
-                passenger_email=email)
-    notifications.send_booking_confirmation_email(booking)
     return render_template("book_ticket.html",
                            train_number=train_number,
                            ticket_class=ticket_class,
                            date=date, email=email)
 
+
 @app.route("/thank-you")
 def thank_you():
     return render_template("thank_you.html")
+
 
 @app.route("/bookings")
 def my_bookings():
@@ -171,11 +178,13 @@ def my_bookings():
     bookings = db.get_trips(email)
     return render_template("bookings.html", bookings=bookings)
 
+
 @app.route("/hello")
 def hello():
     """Simple endpoint to check the authenticated user is
     """
     return render_template("hello.html", user=auth.get_logged_in_user_email())
+
 
 @app.route("/login")
 def login():
@@ -184,6 +193,7 @@ def login():
         return redirect("/hello")
 
     return render_template("login.html")
+
 
 @app.route("/logout")
 def logout():
